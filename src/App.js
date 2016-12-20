da12f57f10e4bbab5bb3994fa0049856431ef6f8
TarikFojnica <tarik.f786@gmail.com>
2016-12-20 16:32:38 +0100

    Test the API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moltin from './moltin';
 
 class App extends Component {
+
+	state = {
+		data: [],
+	};
+
+	componentDidMount() {
+		let _this = this;
+		moltin.Authenticate(function() {
+			_this.setState({
+				data: moltin.Product.List()
+			})
+		});
+	}
+
   render() {
-    return (
+		console.log(this.state.data[0]);
+		let allItems = this.state.data.map(function(result, id) {
+			let images = result.images.map(function(image, imageID) {
+				return (
+					<img src={image.url.http} key={imageID}/>
+				)
+			});
+			return (
+				<div key={id}>
+					{images}
+					<h3 className="uk-panel-title">{result.title}</h3>
+				</div>
+			);
+		});
+
+		return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {}</h2>
+					<button>Log me</button>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+				{allItems}
       </div>
     );
   }
