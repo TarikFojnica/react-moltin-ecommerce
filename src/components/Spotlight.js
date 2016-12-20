275ad50f51f8d299345ac021da7dc171befde45a
TarikFojnica <tarik.f786@gmail.com>
2016-12-21 00:21:26 +0100

    Update Spotlight layout

--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -5,25 +5,21 @@ export default class Spotlight extends React.Component {
 	render() {
 		let allItems = this.props.articles.map(function(result, id) {
 			return (
-				<div key={id} className="column">
+				<div key={id} className="column spotlight-element">
 					<div className="ui card" key={id}>
 						<div className="image">
 							<img src={result.images[0].url.http} />
 						</div>
 						<div className="content">
 							<a className="header">{result.title}</a>
-							<div className="meta">
-								<span className="date">Joined in 2013</span>
-							</div>
 							<div className="description">
 								Kristy is an art director living in New York.
 							</div>
 						</div>
-						<div className="extra content">
-							<a>
-								<i className="user icon" />
-								22 Friends
-							</a>
+						<div className="extra content text-center">
+							<span className="ui button primary">
+								Add To Cart
+							</span>
 						</div>
 					</div>
 				</div>
