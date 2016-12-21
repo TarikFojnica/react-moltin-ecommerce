3d36b7c82833447e7d0f3eff1202c05c8fbd74aa
TarikFojnica <tarik.f786@gmail.com>
2016-12-21 19:19:10 +0100

    Dispay the item image on the Product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,9 +3,15 @@ import moltin from '../vendor/moltin';
 
 export default class Product extends React.Component {
 	state = {
-		product: [],
 		id: this.props.location.pathname.replace('/product/', ''), // remove string '/product/' from the url
-		loaded: false
+		loaded: false,
+		product: {
+			images: [
+				{
+					url: '' // Not sure why, but can't access the images array otherwise. Maybe will investigate. Maybe.
+				}
+			]
+		}
 	};
 
 	componentDidMount() {
@@ -13,17 +19,30 @@ export default class Product extends React.Component {
 
 		moltin.Authenticate(function() {
 			_this.setState({
-				product: moltin.Product.Get(_this.state.id)
+				product: moltin.Product.Get(_this.state.id),
 			})
 		});
 	}
 
 	render() {
+		console.log(this.state.product.images[0].url.http)
+		{/*let image =  this.state.product.images.map(function(result, id) {*/}
+		// 	return(
+		// 		<div>
+		// 			{result}
+		// 		</div>
+		// 	)
+		// });
+
 		return (
 			<div className="product-container">
 				<div className="ui container">
 					<div className="ui grid">
-						<div className="eight wide column"><h1>{this.state.product.title}</h1></div>
+						<div className="eight wide column">
+							<div className="preview-image">
+								<img src={this.state.product.images[0].url.http} alt=""/>
+							</div>
+						</div>
 						<div className="eight wide column">abc</div>
 					</div>
 				</div>
