6e1f1b1b6e2ac4418a391d2fd4dd3e99c1a896b3
TarikFojnica <tarik.f786@gmail.com>
2016-12-21 12:43:43 +0100

    Add products route and component

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Product extends React.Component {
+
+	render() {
+		return (
+			<div className="spotlight-container">
+				<h1>Product</h1>
+			</div>
+		);
+	}
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
-import Home from './components/Home'
+import Home from './components/Home';
+import Product from './components/Product';
 
 const routes = (
 	<Route path="/" component={Layout}>
 		<IndexRoute component={Home}/>
+		<Route path="product" component={Product}/>
 	</Route>
 );
 
