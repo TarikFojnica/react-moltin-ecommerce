4ddb213e826de1211897fe4b6391ace6719b02df
tarikfojnica <tarik.f786@gmail.com>
2017-02-22 22:44:53 +0100

    Add sidebar cart updating icon when adding an item from the Cover

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -29,6 +29,11 @@ export default class Cover extends React.Component {
 
 	addToCart = (clicked) => {
 		let _this = this;
+		// Fire ADD_TO_CART immediately after user initiate the action
+		events.publish('ADD_TO_CART', {
+			adding: true
+		});
+
 		this.setState({
 			clickedId: clicked,
 			adding: true
@@ -36,7 +41,11 @@ export default class Cover extends React.Component {
 
 		moltin.Authenticate(() => {
 			moltin.Cart.Insert(clicked.id, '1', null, function(cart) {
-				events.publish('ADD_TO_CART');
+				// Inform other listeners that ADD_TO_CART event is complete
+				events.publish('ADD_TO_CART', {
+					adding: false
+				});
+
 				_this.setState({
 					adding: false
 				})
