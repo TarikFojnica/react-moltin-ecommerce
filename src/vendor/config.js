7f52c48ae5f2c7a0f2e766fccc40c6a9902f18ce
tarikfojnica <tarik.f786@gmail.com>
2017-03-01 11:36:39 +0100

    Update sidebar design

--- src/vendor/config.js
+++ src/vendor/config.js
@@ -1,7 +1,7 @@
 const config = {
 	publicId: 'd5BY7IIJYsyoSrabACClkacWlB21xCULA53dSMyiX5',
 	// The content from this category is displayed in the upper full width box
-	featuredCategoryId: ''
+	featuredCategoryId: '1415212879321235847'
 };
 
 export default config;
