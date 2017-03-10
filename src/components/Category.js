91bb261dcdeac6551de1e089d350db268ab49c60
tarikfojnica <tarik.f786@gmail.com>
2017-03-11 00:41:24 +0100

    Add data to the category page

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ProductList from '../components/ProductList'
 
 export default class Category extends React.Component {
 	render() {
 		return (
-			<div>
-				<h2>Coming Soon</h2>
+			<div className="category-page">
+				<div className="header">
+					<h1>Men Bags</h1>
+				</div>
+
+				<div className="body">
+					<ProductList size="four"/>
+				</div>
 			</div>
 		);
 	}
