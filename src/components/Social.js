64e8aa02b333f0382b2780483db94f77f4b34cbb
tarikfojnica <tarik.f786@gmail.com>
2017-06-07 00:35:17 +0200

    Change social icons design

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,10 +6,8 @@ export default class Social extends React.Component {
 	render() {
 		return (
 			<div className="social">
-				<ul>
-					<li><a target="_blank" href="https://www.facebook.com/menaccesoriesKanMer/"><i className="facebook icon"></i> Facebook</a></li>
-					<li><a target="_blank" href="http://www.thepicta.com/user/kan.mer/3250353263"><i className="instagram icon"></i> Instagram</a></li>
-				</ul>
+				<a target="_blank" href="https://www.facebook.com/menaccesoriesKanMer/"><i className="facebook icon"></i></a>
+				<a target="_blank" href="http://www.thepicta.com/user/kan.mer/3250353263"><i className="instagram icon"></i></a>
 			</div>
 		);
 	}
